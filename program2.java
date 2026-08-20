9a0c7e30b656d28835ab87f1138b67c3e26d383b
RohanGadhave20 <rohangadhave0204@gmail.com>
2026-08-20 10:15:18 +0530

    Added program2

---
+++ program2.java
@@ -0,0 +1,17 @@
+public class program2 {
+
+    public static void  main(String A[])
+    {
+        int i = 12;
+        int  j = 12;
+        int k = 0;
+
+        k = i + j;
+
+        System.out.println("The sum of i and j is: " + k);
+
+
+    }
+
+    
+}
